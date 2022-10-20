6f5779493fde35af1701c6e083df10837252c352
Brian Clouser <clouser@Brians-MacBook-Pro.local>
2022-10-20 12:00:37 -0400

    added max files on disk fix

--- Sources/Segment/Configuration.swift
+++ Sources/Segment/Configuration.swift
@@ -30,6 +30,7 @@ public class Configuration {
         var autoAddSegmentDestination: Bool = true
         var apiHost: String = HTTPClient.getDefaultAPIHost()
         var cdnHost: String = HTTPClient.getDefaultCDNHost()
+        var maximumLogFilesOnDisk : Int = 15
     }
     internal var values: Values
 
@@ -95,5 +96,11 @@ public extension Configuration {
         values.cdnHost = value
         return self
     }
+    
+    @discardableResult
+     func maximumLogFilesOnDisk(_ value: Int) -> Configuration {
+         values.maximumLogFilesOnDisk = value
+         return self
+     }
 }
 
